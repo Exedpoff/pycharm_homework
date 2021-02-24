51a1d8c74382d90e09355d686685cfd502a9c466
Exedpoff <tron.lpk@yandex.ru>
2021-02-24 12:24:51 +0300

    Add_dz_leson_1

--- Troshin_Ivan_dz_2.py
+++ Troshin_Ivan_dz_2.py
@@ -1,8 +1,27 @@
-numb = int(input("Введите число от 1 до 20: "))
-percent = str("Процент")
-if numb == 1:
-    print(str(numb), percent, sep=' ')
-elif 1 < numb <= 4:
-    print(str(numb), percent + "a", sep=' ')
-else:
-    print(str(numb), percent + "ов", sep=' ')
+list_cube = []
+add_list_cubes = []
+all_sum = 0
+for i in range(1, 1001):
+    add_list_cubes.append(i ** 3)
+
+for ind, val in enumerate(add_list_cubes):
+    sum_digits = 0
+    while val:
+        sum_digits += val % 10
+        val //= 10
+    if sum_digits % 7 == 0:
+        all_sum += add_list_cubes[ind]
+print(all_sum)
+
+
+for m in add_list_cubes:
+    add_list_cubes.append(m + 17)
+    all_sum = 0
+    for ind, val in enumerate(add_list_cubes):
+        sum_digits = 0
+        while val > 0:
+            sum_digits += val % 10
+            val // 10
+            if sum_digits % 7 == 0:
+                all_sum += add_list_cubes[ind]
+print(all_sum)
