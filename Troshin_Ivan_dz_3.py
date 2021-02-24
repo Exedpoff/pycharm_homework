d9a86d1e0ba77501307e472e77afb714fb59a70f
Exedpoff <tron.lpk@yandex.ru>
2021-02-24 10:06:27 +0300

    Troshin_Ivan_dz_2+3

---
+++ Troshin_Ivan_dz_3.py
@@ -0,0 +1,8 @@
+numb = int(input("Введите число от 1 до 20: "))
+percent = str("Процент")
+if numb == 1:
+    print(str(numb),percent,sep=' ')
+elif 1 < numb <= 4:
+    print(str(numb),percent+"a",sep=' ')
+else:
+    print(str(numb),percent+"ов",sep=' ')
